cbc2792939bff94072a34d350e2280753fd66f22
kanadara13 <shin.jaehyeok@leisureq.co.kr>
2018-09-07 23:41:14 +0900

    controller practice

--- src/main/kotlin/com/land/demo/controller/ContractController.kt
+++ src/main/kotlin/com/land/demo/controller/ContractController.kt
@@ -1,15 +1,22 @@
 package com.land.demo.controller
 
-import com.sun.org.apache.xpath.internal.operations.String
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.BodyInserters.fromObject
 import reactor.core.publisher.Mono
+import org.springframework.web.reactive.function.server.ServerResponse
 
-@Controller
+
+@RestController
 class ContractController {
 
+    //@Autowired
+    //lateinit var ContractService contractService
+
     @GetMapping("/api/contract")
-    fun find(): Mono<String>? {
-        return null
+    fun find(): Mono<String> {
+        return Mono.just("contract")
     }
 }
